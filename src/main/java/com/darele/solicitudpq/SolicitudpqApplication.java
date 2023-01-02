47808e5127d6736847d07c7baa9d0379c181631f
D David Galeano P <114103145+DDavidGalP@users.noreply.github.com>
2023-01-02 18:44:56 -0500

    Subida Inicial

---
+++ src/main/java/com/darele/solicitudpq/SolicitudpqApplication.java
@@ -0,0 +1,13 @@
+package com.darele.solicitudpq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitudpqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitudpqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darele/solicitudpq/SolicitudpqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darele.solicitudpq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitudpqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
